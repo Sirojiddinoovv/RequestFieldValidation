0a0332ca0fce2bdf34be794a1800f66bdf1f16f7
Home-PC <davrbankit@gmail.com>
2025-08-09 23:14:06 +0500

    initial commit;

---
+++ src/main/java/uz/nodir/jakartavalidation/JakartaValidationApplication.java
@@ -0,0 +1,13 @@
+package uz.nodir.jakartavalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakartaValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakartaValidationApplication.class, args);
+    }
+
+}
